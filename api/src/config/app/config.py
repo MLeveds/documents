83b0355e598fbcf73995b5d09857709988f1817b
Nikita <tyumin000@gmail.com>
2024-04-06 17:28:34 +0300

    stuff

--- api/src/config/app/config.py
+++ api/src/config/app/config.py
@@ -11,20 +11,12 @@ class AppConfig(BaseSettings):
     APP_VERSION: str
     DEBUG: bool
 
-    UVICORN_APP_NAME: str
-    UVICORN_HOST: str
-    UVICORN_PORT: int
-    UVICORN_RELOAD: bool
-
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
     POSTGRES_PORT: int
     POSTGRES_NAME: str
     POSTGRES_ECHO: str
-
-    JWT_SECRET_KEY: str
-    JWT_ALGORITHM: str
 
     REDIS_PASSWORD: str
     REDIS_HOST: str
