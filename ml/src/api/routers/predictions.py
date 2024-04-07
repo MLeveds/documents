481128cc8e2a79eae5b53ee399afc8725789bad3
Nikita <tyumin000@gmail.com>
2024-04-07 15:38:46 +0300

    migratin'

--- ml/src/api/routers/predictions.py
+++ ml/src/api/routers/predictions.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Request
 from src.api.responses.api_response import ApiResponse
+import asyncio
 
 router = APIRouter()
 
 
 @router.post("/predict")
 async def predict(request: Request):
+    await asyncio.sleep(5)
     return ApiResponse.payload({'message': 'hellow'})
