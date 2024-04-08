feae9135f55c9aa1166e279374cafc190d793448
Nikita <tyumin000@gmail.com>
2024-04-08 16:01:40 +0300

    front

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,7 +2,25 @@
 </script>
 
 <template>
+    <router-view/>
 </template>
 
-<style scoped>
+<style>
+/*@font-face{*/
+/*    font-family:SansSerif;*/
+/*    src: url(fonts/myfont.ttf)*/
+/*}*/
+:root {
+    --purple: #E8D3FF;
+    --blue: #C6CFFF;
+    --lblue: #DEECFF;
+    --white: #F3F8FF;
+}
+html, body {
+    margin: 0;
+    padding: 0;
+    font-family: Sans-Serif,serif;
+    background-color: var(--white);
+}
+/*var(--blue)*/
 </style>
---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,86 @@
+<template>
+    <header class="header">
+        <span class="header__logo">Documento</span>
+    </header>
+    <div class="main">
+        <div class="main__documents-list">
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+            <div class="main__documents-list-item"></div>
+        </div>
+        <div class="main__documents-view">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header {
+    display: flex;
+    align-content: flex-start;
+    align-items: center;
+    width: 100%;
+    height: 7vh;
+    background-color: var(--purple);
+}
+.main {
+    display:flex;
+    width: 100%;
+    min-height: 93vh;
+}
+.main__documents-list {
+    width: 40%;
+    border-radius: 50px;
+    margin: 10px 10px 10px 10px;
+    padding-bottom: 10px;
+    background-color: var(--lblue);
+    box-shadow: 5px 5px 10px var(--blue);
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    overflow: auto;
+}
+.main__documents-list-item {
+    box-sizing: border-box;
+    width: 90%;
+    /*margin: 0 5% 0 5%;*/
+    margin-top: 10px;
+    height: 50px;
+    border-radius: 50px;
+    background-color: var(--white);
+    background-clip: content-box;
+}
+.main__documents-view {
+    width: 60%;
+    max-height: 75vh;
+    border-radius: 50px;
+    margin: 10px 10px 10px 0;
+    background-color: var(--lblue);
+    box-shadow: 5px 5px 10px var(--blue);
+}
+.header__logo {
+    padding-left: 10px;
+}
+</style>
